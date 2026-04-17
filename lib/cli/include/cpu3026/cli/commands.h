e4698d5fa00023ea1014e3d4572e22d7479f0a13
Enblightener <acharris983@gmail.com>
2026-04-17 13:24:10 -0400

    Added new commands

--- lib/cli/include/cpu3026/cli/commands.h
+++ lib/cli/include/cpu3026/cli/commands.h
@@ -5,6 +5,9 @@
 #include "command_handler.h"
 #include "commands/load_cmd.h"
 #include "commands/run_cmd.h"
+#include "commands/break_cmd.h"
+#include "commands/interrupt_cmd.h"
+#include "commands/mem_cmd.h"
 #include <cpu3026/vm/vm.h>
 
 namespace cpu3026 {
@@ -16,6 +19,12 @@ namespace cpu3026 {
 
 		o.emplace("load", &commands::load_cmd);
 		o.emplace("run", &commands::run_cmd);
+		o.emplace("break", &commands::break_cmd);
+		o.emplace("interrupt", &commands::interrupt_cmd);
+		o.emplace("dump", &commands::dump_cmd);
+		o.emplace("getb", &commands::getb_cmd);
+		o.emplace("setb", &commands::setb_cmd);
+		o.emplace("disasm", &commands::disasm_cmd);
 
 		return o;
 	}
